afc1b5bdf1bcd03a49585a097d0f82a749d434f8
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-07 17:20:57 +0100

    0.1

--- app/src/main/java/com/example/myapplication/ui/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/HomeFragment.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication.ui
 
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
+
+   private lateinit var binding : HomeFragmentBinding
+
 }
+
