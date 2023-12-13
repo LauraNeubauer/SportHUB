fa8c09281d5cb36e51214c991bfa56d37a374ff8
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-13 15:14:46 +0100

    0.3 -nav funktionsfähig

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -14,24 +14,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //  setContentView(binding.root)
-
-        window.statusBarColor = ContextCompat.getColor(this, R.color.tags)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val navHost =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        setContentView(binding.root)
 
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         binding.bnv.setupWithNavController(navHost.navController)
 
-    /*    navHost.navController.addOnDestinationChangedListener { _, destination, _ ->
+        navHost.navController.addOnDestinationChangedListener{_,destination,_ ->
             when (destination.id) {
-                R.id.homeFragment -> binding.bnv.visibility = View.GONE
                 R.id.profilFragment -> binding.bnv.visibility = View.GONE
                 else -> binding.bnv.visibility = View.VISIBLE
-            }
-        }  */
 
+            }            }
     }
 }
