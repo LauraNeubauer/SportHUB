9a89a54af8c1df0504fcc648d2a37b1137e72dbb
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-13 19:27:02 +0100

    0.3 -onboarding 1 & 2 design & ui

---
+++ app/src/main/java/com/example/myapplication/ui/OnBoardingTwoFragment.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.OnboardingTwoFragmentBinding
+
+class OnBoardingTwoFragment : Fragment() {
+
+    private lateinit var binding : OnboardingTwoFragmentBinding
+
+}
