992a128bb14ba91f0c7a472ce9f48ebbbf428804
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-14 12:31:02 +0100

    0.4 -result_list_item_small

--- app/src/main/java/com/example/myapplication/adapter/ResultsAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/ResultsAdapter.kt
@@ -3,33 +3,30 @@ package com.example.myapplication.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.GameListItemBinding
+import com.example.myapplication.databinding.ResultsListItemSmallBinding
 import com.example.myapplication.model.Results
 
 class ResultsAdapter(
     private val dataset: List<Results>
 ) : RecyclerView.Adapter<ResultsAdapter.ItemViewHolder>() {
 
-    inner class ItemViewHolder(val binding: GameListItemBinding) :
+    inner class ItemViewHolder(val binding: ResultsListItemSmallBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ResultsAdapter.ItemViewHolder {
-        val binding = GameListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ResultsListItemSmallBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
 
-        holder.binding.tvDate.text = item.date
-        holder.binding.shots.text = item.shots
-        holder.binding.tvLvl.text = item.mainLvl
-        holder.binding.tvLvlOpps.text = item.oppsLvl
-        holder.binding.tvNameMy.text = item.mainPlayer
-        holder.binding.tvNameOpp.text = item.playerOpps
+        holder.binding.tvGroupOne.text = item.mainPlayer
+        holder.binding.tvGroupTwo.text = item.playerOpps
+        holder.binding.tvLand.text = item.place
 
     }
 
