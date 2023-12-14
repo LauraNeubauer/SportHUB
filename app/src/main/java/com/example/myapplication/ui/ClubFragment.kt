f70357df49067ac5233a4ec1a852036634473cfe
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-14 17:12:44 +0100

    0.4 - cleaned - Profil aktualisiert - club aktualisiert

--- app/src/main/java/com/example/myapplication/ui/ClubFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/ClubFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.adapter.ClubAdapter
 import com.example.myapplication.data.ExampleDatabase
 import com.example.myapplication.databinding.ClubFragmentBinding
 
@@ -26,6 +25,5 @@ class ClubFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.rvClub.adapter = ClubAdapter(dataset)
     }
 }
