f79e054d87cbb449dd75bca6e6a5d9185654bc94
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-21 00:28:52 +0100

    1.3 - calender

--- app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
@@ -15,13 +15,14 @@ import com.example.myapplication.R
 import com.example.myapplication.adapter.EventHomeAdapter
 import com.example.myapplication.data.ExampleDatabase
 import com.example.myapplication.databinding.CalenderFragmentBinding
+import java.text.SimpleDateFormat
 import java.util.Calendar
 
 class CalenderFragment : Fragment() {
 
-    private lateinit var binding : CalenderFragmentBinding
+    private lateinit var binding: CalenderFragmentBinding
     var datasetEvents = ExampleDatabase().loadEvents()
-    private val viewModel : PersonViewModel by activityViewModels()
+    private val viewModel: PersonViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -42,7 +43,85 @@ class CalenderFragment : Fragment() {
         binding.ddBtSortCal.setOnClickListener {
             showPopupMenuSortCal(it)
         }
+        weekdays()
+    }
 
+    private fun weekdays() {
+
+        val currentDate = Calendar.getInstance().time
+        val calendar = Calendar.getInstance()
+        calendar.time = currentDate
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+        val dayOfWeekString = SimpleDateFormat("EEEE").format(calendar.time)
+
+        when (dayOfWeekString) {
+            "Monday" -> {
+                binding.dateOne.text = dayOfMonth.toString()
+                binding.dateTwo.text = (dayOfMonth.plus(1).toString())
+                binding.dateThree.text = (dayOfMonth.plus(2).toString())
+                binding.dateFour.text = (dayOfMonth.plus(3).toString())
+                binding.dateFive.text = (dayOfMonth.plus(4).toString())
+                binding.dateSix.text = (dayOfMonth.plus(5).toString())
+                binding.dateSeven.text = (dayOfMonth.plus(6).toString())
+            }
+            "Tuesday" -> {
+                binding.dateOne.text = (dayOfMonth - 1).toString()
+                binding.dateTwo.text = dayOfMonth.toString()
+                binding.dateThree.text = (dayOfMonth.plus(1).toString())
+                binding.dateFour.text = (dayOfMonth.plus(2).toString())
+                binding.dateFive.text = (dayOfMonth.plus(3).toString())
+                binding.dateSix.text = (dayOfMonth.plus(4).toString())
+                binding.dateSeven.text = (dayOfMonth.plus(5).toString())
+            }
+            "Wednesday" -> {
+                binding.dateOne.text = (dayOfMonth - 2).toString()
+                binding.dateTwo.text = (dayOfMonth - 1).toString()
+                binding.dateThree.text = dayOfMonth.toString()
+                binding.dateFour.text = (dayOfMonth.plus(1).toString())
+                binding.dateFive.text = (dayOfMonth.plus(2).toString())
+                binding.dateSix.text = (dayOfMonth.plus(3).toString())
+                binding.dateSeven.text = (dayOfMonth.plus(4).toString())
+            }
+            "Thursday" -> {
+                binding.dateOne.text = (dayOfMonth - 3).toString()
+                binding.dateTwo.text = (dayOfMonth - 2).toString()
+                binding.dateThree.text = (dayOfMonth - 1).toString()
+                binding.dateFour.text = dayOfMonth.toString()
+                binding.dateFive.text = (dayOfMonth.plus(1).toString())
+                binding.dateSix.text = (dayOfMonth.plus(2).toString())
+                binding.dateSeven.text = (dayOfMonth.plus(3).toString())
+            }
+            "Friday" -> {
+                binding.dateOne.text = (dayOfMonth - 4).toString()
+                binding.dateTwo.text = (dayOfMonth - 3).toString()
+                binding.dateThree.text = (dayOfMonth - 2).toString()
+                binding.dateFour.text = (dayOfMonth - 1).toString()
+                binding.dateFive.text = dayOfMonth.toString()
+                binding.dateSix.text = (dayOfMonth.plus(5).toString())
+                binding.dateSeven.text = (dayOfMonth.plus(6).toString())
+            }
+            "Saturday" -> {
+                binding.dateOne.text = (dayOfMonth - 5).toString()
+                binding.dateTwo.text = (dayOfMonth - 4).toString()
+                binding.dateThree.text = (dayOfMonth - 3).toString()
+                binding.dateFour.text = (dayOfMonth - 2).toString()
+                binding.dateFive.text = (dayOfMonth - 1).toString()
+                binding.dateSix.text = dayOfMonth.toString()
+                binding.dateSeven.text = (dayOfMonth.plus(1).toString())
+            }
+            "Sunday" -> {
+                binding.dateThree.text = (dayOfMonth - 6).toString()
+                binding.dateThree.text = (dayOfMonth - 5).toString()
+                binding.dateThree.text = (dayOfMonth - 4).toString()
+                binding.dateThree.text = (dayOfMonth - 3).toString()
+                binding.dateThree.text = (dayOfMonth - 2).toString()
+                binding.dateThree.text = (dayOfMonth - 1).toString()
+                binding.dateOne.text = dayOfMonth.toString()
+            }
+            else -> {
+            }
+        }
     }
 
     private fun showPopupMenuSortCal(view: View) {
@@ -56,26 +135,32 @@ class CalenderFragment : Fragment() {
                     binding.ddBtSortCal.text = "DIESE WOCHE"
                     true
                 }
+
                 R.id.option2 -> {
                     binding.ddBtSortCal.text = "NÄCHSTE WOCHE"
                     true
                 }
+
                 R.id.option3 -> {
                     binding.ddBtSortCal.text = "IN 2 WOCHEN"
                     true
                 }
+
                 R.id.option4 -> {
                     binding.ddBtSortCal.text = "IN 3 WOCHEN"
                     true
                 }
+
                 R.id.option5 -> {
                     binding.ddBtSortCal.text = "NÄCHSTEN MONAT"
                     true
                 }
+
                 R.id.option6 -> {
                     showDatePickerDialog()
                     true
                 }
+
                 else -> false
             }
         }
@@ -94,8 +179,13 @@ class CalenderFragment : Fragment() {
             R.style.DatePickerDialogTheme, // Hier wird der benutzerdefinierte Stil angewendet
             DatePickerDialog.OnDateSetListener { view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
                 // Hier wird das ausgewählte Datum verarbeitet
-                val selectedDate = "$dayOfMonth.${monthOfYear + 1}.$year"
+                val selectedDate = if (dayOfMonth < 10) {
+                    "0$dayOfMonth.${monthOfYear + 1}.$year"
+                } else {
+                    "$dayOfMonth.${monthOfYear + 1}.$year"
+                }
                 binding.ddBtSortCal.text = selectedDate
+
             },
             year,
             month,
