33d82ad12af57ffea37d597135071145a4ca4c6f
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-23 20:50:11 +0100

    1.5 - login popup

--- app/src/main/java/com/example/myapplication/ui/OnBoardingTwoFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/OnBoardingTwoFragment.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -27,9 +30,6 @@ class OnBoardingTwoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var email = binding.tietEmail.text.toString()
-        var passwort = binding.tierPassword.text.toString()
-
         firebaseViewModel.currentUser.observe(viewLifecycleOwner) {
             if (it != null) {
                 findNavController().navigate(R.id.homeFragment)
@@ -37,11 +37,44 @@ class OnBoardingTwoFragment : Fragment() {
         }
 
         binding.btLogin.setOnClickListener {
-            firebaseViewModel.login(email, passwort)
+            var email = binding.tietEmail.text.toString()
+            var passwort = binding.tierPassword.text.toString()
+
+            if (binding.tietEmail.text!!.isEmpty() && binding.tierPassword.text!!.isEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("LogIn - Daten fehlen", "Geben Sie die Email-Adresse und das Passwort ein, bei dem Sie sich bei SportHUB registriert haben")
+                binding.btLogin.isClickable = false
+            } else if (binding.tietEmail.text!!.isNotEmpty() && binding.tierPassword.text!!.isEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("LogIn - Daten fehlen", "Geben Sie das Passwort ein, bei dem Sie sich bei SportHUB registriert haben")
+                binding.btLogin.isClickable = false
+            } else if (binding.tietEmail.text!!.isEmpty() && binding.tierPassword.text!!.isNotEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("LogIn - Daten fehlen", "Geben Sie die Email-Adresse ein, bei dem Sie sich bei SportHUB registriert haben")
+                binding.btLogin.isClickable = false
+            } else {
+                firebaseViewModel.login(email, passwort)
+            }
         }
 
         binding.reset.setOnClickListener {
+            var email = binding.tietEmail.text.toString()
+
             firebaseViewModel.sendPasswordReset(email)
         }
+    }
+    private fun showPopUp(titel: String, nachricht: String) {
+        val builder = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
+        builder.setTitle(titel)
+        builder.setMessage(nachricht)
+        builder.setPositiveButton("OK") { dialog, which ->
+
+            binding.btLogin.isClickable = true
+            dialog.dismiss()
+            binding.textInputLayout4.visibility = VISIBLE
+        }
+        builder.setCancelable(false)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
