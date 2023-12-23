5646209cfb7a35a1e50723aca62503698361488d
lauraneubauer <malenundzeichnen@hotmail.de>
2023-12-23 23:47:19 +0100

    1.5 - registrate popup

--- app/src/main/java/com/example/myapplication/ui/RegistrateFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/RegistrateFragment.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication.ui
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -36,11 +38,38 @@ class RegistrateFragment : Fragment() {
         }
 
         binding.btRegistrate.setOnClickListener {
-           var email = binding.tietEmail.text.toString()
-           var passwort = binding.tierPassword.text.toString()
+            var email = binding.tietEmail.text.toString()
+            var passwort = binding.tierPassword.text.toString()
 
-            firebaseViewModel.register(email, passwort)
+            if (binding.tietEmail.text!!.isEmpty() && binding.tierPassword.text!!.isEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("Daten fehlen", "Bitte geben Sie eine gültige Email-Adresse und ein Passwort mit mindestens 6 Zeichen ein")
+                binding.btRegistrate.isClickable = false
+            } else if (binding.tietEmail.text!!.isNotEmpty() && binding.tierPassword.text!!.isEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("Daten fehlen", "Bitte geben Sie zum Registrieren ein Passwort mit mindestens 6 Zeichen ein")
+                binding.btRegistrate.isClickable = false
+            } else if (binding.tietEmail.text!!.isEmpty() && binding.tierPassword.text!!.isNotEmpty()) {
+                binding.textInputLayout4.visibility = INVISIBLE
+                showPopUp("Daten fehlen", "Bitte geben Sie zum Registrieren eine gültige Email-Adresse ein")
+                binding.btRegistrate.isClickable = false
+            } else {
+                firebaseViewModel.register(email, passwort)
+            }
         }
     }
+    private fun showPopUp(titel: String, nachricht: String) {
+        val builder = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
+        builder.setTitle(titel)
+        builder.setMessage(nachricht)
+        builder.setPositiveButton("OK") { dialog, which ->
 
+            binding.btRegistrate.isClickable = true
+            dialog.dismiss()
+            binding.textInputLayout4.visibility = View.VISIBLE
+        }
+        builder.setCancelable(false)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 }
