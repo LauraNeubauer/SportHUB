07128f0604f04a221f021dd8171b8654ed54113d
lauraneubauer <malenundzeichnen@hotmail.de>
2024-01-18 13:01:48 +0100

    4.4 - events item anpassungen

--- app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
@@ -1,19 +1,14 @@
 package com.example.myapplication.ui
 
 import android.annotation.SuppressLint
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.myapplication.PersonApi.ViewModel
-import com.example.myapplication.R
 import com.example.myapplication.adapter.EventHomeAdapter
 import com.example.myapplication.data.ExampleDatabase
 import com.example.myapplication.databinding.CalenderFragmentBinding
@@ -46,10 +41,6 @@ class CalenderFragment : Fragment() {
 
         binding.rvEvents.adapter = EventHomeAdapter(datasetEvents, viewModel)
 
-        binding.ddBtSortCal.text = "SORT"
-        binding.ddBtSortCal.setOnClickListener {
-            showPopupMenuSortCal(it)
-        }
         weekdays()
     }
 
@@ -63,7 +54,8 @@ class CalenderFragment : Fragment() {
         val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
 
         fun setBindingText(binding: TextView, day: Int) {
-            val wrappedDay = (day + lastDayOfMonth) % lastDayOfMonth // Ensure day is within 1 to lastDayOfMonth
+            val wrappedDay =
+                (day + lastDayOfMonth) % lastDayOfMonth // Ensure day is within 1 to lastDayOfMonth
             binding.text = if (wrappedDay == 0) lastDayOfMonth.toString() else wrappedDay.toString()
         }
         when (SimpleDateFormat("EEEE").format(calendar.time)) {
@@ -141,75 +133,5 @@ class CalenderFragment : Fragment() {
                 // Handle other cases if needed
             }
         }
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun showPopupMenuSortCal(view: View) {
-        val popupMenu = PopupMenu(requireContext(), view, 0, 2, R.style.PopupMenu)
-        val menuInflater = popupMenu.menuInflater
-        menuInflater.inflate(R.menu.dd_menu_sort_cal, popupMenu.menu)
-
-        popupMenu.setOnMenuItemClickListener { item: MenuItem? ->
-            when (item?.itemId) {
-                R.id.option1 -> {
-                    binding.ddBtSortCal.text = "DIESE WOCHE"
-                    true
-                }
-
-                R.id.option2 -> {
-                    binding.ddBtSortCal.text = "NÄCHSTE WOCHE"
-                    true
-                }
-
-                R.id.option3 -> {
-                    binding.ddBtSortCal.text = "IN 2 WOCHEN"
-                    true
-                }
-
-                R.id.option4 -> {
-                    binding.ddBtSortCal.text = "IN 3 WOCHEN"
-                    true
-                }
-
-                R.id.option5 -> {
-                    binding.ddBtSortCal.text = "NÄCHSTEN MONAT"
-                    true
-                }
-
-                R.id.option6 -> {
-                    showDatePickerDialog()
-                    true
-                }
-
-                else -> false
-            }
-        }
-
-        popupMenu.show()
-    }
-
-    private fun showDatePickerDialog() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val datePickerDialog = DatePickerDialog(
-            requireContext(),
-            R.style.DatePickerDialogTheme,
-            { _: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-                val selectedDate = if (dayOfMonth < 10) {
-                    "0$dayOfMonth.${monthOfYear + 1}.$year"
-                } else {
-                    "$dayOfMonth.${monthOfYear + 1}.$year"
-                }
-                binding.ddBtSortCal.text = selectedDate
-
-            },
-            year,
-            month,
-            day
-        )
-        datePickerDialog.show()
     }
 }
