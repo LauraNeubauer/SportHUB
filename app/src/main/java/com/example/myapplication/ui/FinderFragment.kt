569f4d8ad1a3580b80e5045e33ff6e0088da9e48
lauraneubauer <malenundzeichnen@hotmail.de>
2024-01-18 14:16:37 +0100

    4.4 - finder popUp start

--- app/src/main/java/com/example/myapplication/ui/FinderFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/FinderFragment.kt
@@ -3,10 +3,12 @@ package com.example.myapplication.ui
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -69,6 +71,10 @@ class FinderFragment : Fragment() {
 
         binding.ddBtSort.text = "SORT"
         binding.ddBtSort.isClickable = false
+
+        if (binding.ddbtSearch.text == "SEARCH") {
+            showPopUpStart("FINDER", "Hier können Sie mittels Filter nach passenden Matches sowie nach Vereinen in der Datenbank suchen")
+        }
 
         binding.ddbtSearch.setOnClickListener {
             showPopupMenuSearch(it)
@@ -369,6 +375,25 @@ class FinderFragment : Fragment() {
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+    private fun showPopUpStart(titel: String, nachricht: String) {
+        val builder = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
+        builder.setTitle(titel)
+        builder.setMessage(nachricht)
+        builder.setPositiveButton("OK") { dialog, which ->
+            dialog.dismiss()
+        }
+        builder.setCancelable(false)
+        val dialog: AlertDialog = builder.create()
+        val window: Window? = dialog.window
+        val layoutParams = window?.attributes
+        layoutParams?.gravity = Gravity.BOTTOM // Setzen Sie die gewünschte Gravity
 
+        val displayMetrics = resources.displayMetrics
+        layoutParams?.height = (displayMetrics.heightPixels * 2) / 4
+        layoutParams?.y = (displayMetrics.heightPixels - layoutParams!!.height) / 2
+        window?.attributes = layoutParams
+
+        dialog.show()
+    }
 
 }
