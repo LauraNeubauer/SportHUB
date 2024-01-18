93bb1000e8af57abce3ec224f3e44af8fd4d5f30
lauraneubauer <malenundzeichnen@hotmail.de>
2024-01-18 12:41:17 +0100

    4.4 - cal fragment dates before/after 31

--- app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/CalenderFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.PopupMenu
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.myapplication.PersonApi.ViewModel
@@ -58,73 +59,85 @@ class CalenderFragment : Fragment() {
         val currentDate = Calendar.getInstance().time
         val calendar = Calendar.getInstance()
         calendar.time = currentDate
-        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
 
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        fun setBindingText(binding: TextView, day: Int) {
+            val wrappedDay = (day + 31) % 31 // Ensure day is within 1 to 31
+            binding.text = if (wrappedDay == 0) "31" else wrappedDay.toString()
+        }
         when (SimpleDateFormat("EEEE").format(calendar.time)) {
             "Monday" -> {
-                binding.dateOne.text = dayOfMonth.toString()
-                binding.dateTwo.text = (dayOfMonth.plus(1).toString())
-                binding.dateThree.text = (dayOfMonth.plus(2).toString())
-                binding.dateFour.text = (dayOfMonth.plus(3).toString())
-                binding.dateFive.text = (dayOfMonth.plus(4).toString())
-                binding.dateSix.text = (dayOfMonth.plus(5).toString())
-                binding.dateSeven.text = (dayOfMonth.plus(6).toString())
+                setBindingText(binding.dateOne, dayOfMonth)
+                setBindingText(binding.dateTwo, dayOfMonth + 1)
+                setBindingText(binding.dateThree, dayOfMonth + 2)
+                setBindingText(binding.dateFour, dayOfMonth + 3)
+                setBindingText(binding.dateFive, dayOfMonth + 4)
+                setBindingText(binding.dateSix, dayOfMonth + 5)
+                setBindingText(binding.dateSeven, dayOfMonth + 6)
             }
+
             "Tuesday" -> {
-                binding.dateOne.text = (dayOfMonth - 1).toString()
-                binding.dateTwo.text = dayOfMonth.toString()
-                binding.dateThree.text = (dayOfMonth.plus(1).toString())
-                binding.dateFour.text = (dayOfMonth.plus(2).toString())
-                binding.dateFive.text = (dayOfMonth.plus(3).toString())
-                binding.dateSix.text = (dayOfMonth.plus(4).toString())
-                binding.dateSeven.text = (dayOfMonth.plus(5).toString())
+                setBindingText(binding.dateOne, dayOfMonth - 1)
+                setBindingText(binding.dateTwo, dayOfMonth)
+                setBindingText(binding.dateThree, dayOfMonth + 1)
+                setBindingText(binding.dateFour, dayOfMonth + 2)
+                setBindingText(binding.dateFive, dayOfMonth + 3)
+                setBindingText(binding.dateSix, dayOfMonth + 4)
+                setBindingText(binding.dateSeven, dayOfMonth + 5)
             }
+
             "Wednesday" -> {
-                binding.dateOne.text = (dayOfMonth - 2).toString()
-                binding.dateTwo.text = (dayOfMonth - 1).toString()
-                binding.dateThree.text = dayOfMonth.toString()
-                binding.dateFour.text = (dayOfMonth.plus(1).toString())
-                binding.dateFive.text = (dayOfMonth.plus(2).toString())
-                binding.dateSix.text = (dayOfMonth.plus(3).toString())
-                binding.dateSeven.text = (dayOfMonth.plus(4).toString())
+                setBindingText(binding.dateOne, dayOfMonth - 2)
+                setBindingText(binding.dateTwo, dayOfMonth - 1)
+                setBindingText(binding.dateThree, dayOfMonth)
+                setBindingText(binding.dateFour, dayOfMonth + 1)
+                setBindingText(binding.dateFive, dayOfMonth + 2)
+                setBindingText(binding.dateSix, dayOfMonth + 3)
+                setBindingText(binding.dateSeven, dayOfMonth + 4)
             }
+
             "Thursday" -> {
-                binding.dateOne.text = (dayOfMonth - 3).toString()
-                binding.dateTwo.text = (dayOfMonth - 2).toString()
-                binding.dateThree.text = (dayOfMonth - 1).toString()
-                binding.dateFour.text = dayOfMonth.toString()
-                binding.dateFive.text = (dayOfMonth.plus(1).toString())
-                binding.dateSix.text = (dayOfMonth.plus(2).toString())
-                binding.dateSeven.text = (dayOfMonth.plus(3).toString())
+                setBindingText(binding.dateOne, dayOfMonth - 3)
+                setBindingText(binding.dateTwo, dayOfMonth - 2)
+                setBindingText(binding.dateThree, dayOfMonth - 1)
+                setBindingText(binding.dateFour, dayOfMonth)
+                setBindingText(binding.dateFive, dayOfMonth + 1)
+                setBindingText(binding.dateSix, dayOfMonth + 2)
+                setBindingText(binding.dateSeven, dayOfMonth + 3)
             }
+
             "Friday" -> {
-                binding.dateOne.text = (dayOfMonth - 4).toString()
-                binding.dateTwo.text = (dayOfMonth - 3).toString()
-                binding.dateThree.text = (dayOfMonth - 2).toString()
-                binding.dateFour.text = (dayOfMonth - 1).toString()
-                binding.dateFive.text = dayOfMonth.toString()
-                binding.dateSix.text = (dayOfMonth.plus(5).toString())
-                binding.dateSeven.text = (dayOfMonth.plus(6).toString())
+                setBindingText(binding.dateOne, dayOfMonth - 4)
+                setBindingText(binding.dateTwo, dayOfMonth - 3)
+                setBindingText(binding.dateThree, dayOfMonth - 2)
+                setBindingText(binding.dateFour, dayOfMonth - 1)
+                setBindingText(binding.dateFive, dayOfMonth)
+                setBindingText(binding.dateSix, dayOfMonth + 5)
+                setBindingText(binding.dateSeven, dayOfMonth + 6)
             }
+
             "Saturday" -> {
-                binding.dateOne.text = (dayOfMonth - 5).toString()
-                binding.dateTwo.text = (dayOfMonth - 4).toString()
-                binding.dateThree.text = (dayOfMonth - 3).toString()
-                binding.dateFour.text = (dayOfMonth - 2).toString()
-                binding.dateFive.text = (dayOfMonth - 1).toString()
-                binding.dateSix.text = dayOfMonth.toString()
-                binding.dateSeven.text = (dayOfMonth.plus(1).toString())
+                setBindingText(binding.dateOne, dayOfMonth - 5)
+                setBindingText(binding.dateTwo, dayOfMonth - 4)
+                setBindingText(binding.dateThree, dayOfMonth - 3)
+                setBindingText(binding.dateFour, dayOfMonth - 2)
+                setBindingText(binding.dateFive, dayOfMonth - 1)
+                setBindingText(binding.dateSix, dayOfMonth)
+                setBindingText(binding.dateSeven, dayOfMonth + 1)
             }
+
             "Sunday" -> {
-                binding.dateThree.text = (dayOfMonth - 6).toString()
-                binding.dateThree.text = (dayOfMonth - 5).toString()
-                binding.dateThree.text = (dayOfMonth - 4).toString()
-                binding.dateThree.text = (dayOfMonth - 3).toString()
-                binding.dateThree.text = (dayOfMonth - 2).toString()
-                binding.dateThree.text = (dayOfMonth - 1).toString()
-                binding.dateOne.text = dayOfMonth.toString()
+                setBindingText(binding.dateOne, dayOfMonth - 6)
+                setBindingText(binding.dateTwo, dayOfMonth - 5)
+                setBindingText(binding.dateThree, dayOfMonth - 4)
+                setBindingText(binding.dateFour, dayOfMonth - 3)
+                setBindingText(binding.dateFive, dayOfMonth - 2)
+                setBindingText(binding.dateSix, dayOfMonth - 1)
+                setBindingText(binding.dateSeven, dayOfMonth)
             }
+
             else -> {
+                // Handle other cases if needed
             }
         }
     }
